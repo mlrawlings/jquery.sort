45d4304bb9a352bfa9c934b97d5dc6b4d4dc5430
Michael Rawlings <mlrawlings@embarkweb.com>
2013-02-18 23:50:56 -0500

    use .sortChildren()
    use .sortChildren() so that we don't override the built-in .sort() function

--- jquery.sort.js
+++ jquery.sort.js
@@ -1,52 +1,44 @@
 //jquery.sort.js
 (function( $ ) {
-  
+	
 	var defaultSortFunction = function( a, b ) {
 		return $( a ).text().localeCompare( $( b ).text() );
 	};
 
 	// sort using binary search insertion O(n(log n))
-	$.fn.sort = function( fn ) {
+	$.fn.sortChildren = function( fn ) {
 		var sorter = fn || defaultSortFunction;
-		var sorted = $('<div>');
 		this.data( 'sortFunction', sorter );
-		sorted.data( 'sortFunction', sorter );
-		this.children().each( function( index, child ) {
-			sorted.insertSorted( child );
-		});
-		this.append( sorted.children() );
+		this.children().sort( sorter ).appendTo( this );
 		return this;
 	};
 
 	// binary search insertion O(log n)
 	$.fn.insertSorted = function( toInsert ) {
-		var sorter = this.data( 'sortFunction' );
-		var children = this.children();
+		var sorter, children, startIndex, stopIndex, middle, inserted;
+		sorter = this.data( 'sortFunction' );
+		children = this.children();
 		if( sorter && children.length > 0 ) {
-			var startIndex = 0;
-			var stopIndex  = children.length - 1;
-		    var middle     = Math.floor( (stopIndex + startIndex) / 2 );
-		    var inserted   = false;
-			while( inserted == false ) {
+			startIndex = 0;
+			stopIndex  = children.length - 1;
+		    middle     = Math.floor( (stopIndex + startIndex) / 2 );
+			while( !inserted ) {
 				var element = children.get( middle );
 				var compare = sorter( toInsert, element )
 				if( compare < 0 ) {
 		        	stopIndex = middle - 1;
 		        	if( middle == startIndex ) {
-		        		$( element ).before( toInsert );
-		        		inserted = true;
+		        		inserted = $( element ).before( toInsert );
 		        	}
 		        }
 		        else if ( compare > 0 ) {
 		        	startIndex = middle + 1;
 		        	if( middle == stopIndex ) {
-		        		$( element ).after( toInsert );
-		        		inserted = true;
+		        		inserted = $( element ).after( toInsert );
 		        	}
 		        }
 		        else {
-		        	$( element ).after( toInsert );
-		        	inserted = true;
+		        	inserted = $( element ).after( toInsert );
 		        }
 		        middle = Math.floor( (stopIndex + startIndex) / 2 );
 			}
